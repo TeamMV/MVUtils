cff40ff9b50982d51b338a653d2fe01e17537ab7
mqxf <max_savenkov@mail.ru>
2023-04-12 11:35:51 +0200

    Try catch

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,13 @@ pub mod args;
 
 #[cfg(test)]
 mod tests {
-    use std::env;
-    use crate::args::ParseArgs;
     use crate::try_catch;
 
     #[test]
     fn it_works() {
         let value = try_catch!({
-            let myRes: Result<i32, String> = Err("hello".to_string());
-            myRes
+            let my_res: Result<i32, String> = Err("hello".to_string());
+            my_res
         }, |e| {
             println!("{}", e);
         });
