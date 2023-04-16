cc50aa1fb5f3dd654c0673d2f50907f78efaa25c
v22lel <v22julian@gmail.com>
2023-04-16 23:05:24 +0200

    minor fix

--- src/utils.rs
+++ src/utils.rs
@@ -249,7 +249,7 @@ macro_rules! impl_incr {
     };
 }
 
-impl_incr!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, f32, f64)
+impl_incr!(i8, i16, i32, i64, i128, u8, u16, u32, u64, u128, f32, f64);
 
 #[macro_export]
 macro_rules ! init_arr {
