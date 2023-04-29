55741d0fb41942946f4f80082c2dda7008d07e6c
Maxim Savenkov <max_savenkov@mail.ru>
2023-04-29 13:59:30 +0200

    Measurement

--- src/screen.rs
+++ src/screen.rs
@@ -2,13 +2,13 @@ pub struct Measurements;
 impl Measurements {
     pub fn compute(dpi: f32, value: f32, me: &Measurement) -> f32 {
         match me {
-            Measurement::PX => { value }
-            Measurement::MM => { dpi / 25.4 * value}
-            Measurement::CM => { dpi / 2.54 * value}
-            Measurement::DM => { dpi / 0.254 * value}
-            Measurement::M => { dpi / 0.0254 * value}
-            Measurement::IN => { dpi * value}
-            Measurement::FT => { dpi * value / 12.0}
+            Measurement::PX => value,
+            Measurement::MM => dpi / 25.4 * value,
+            Measurement::CM => dpi / 2.54 * value,
+            Measurement::DM => dpi / 0.254 * value,
+            Measurement::M => dpi / 0.0254 * value,
+            Measurement::IN => dpi * value,
+            Measurement::FT => dpi * value / 12.0
         }
     }
 }
