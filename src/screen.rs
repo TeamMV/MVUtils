413b562a617bddbeb2c7ea6e60a0e66de4f7a4df
v22lel <v22julian@gmail.com>
2023-04-29 16:59:02 +0200

    Measurement fix

--- src/screen.rs
+++ src/screen.rs
@@ -1,19 +1,6 @@
-pub struct Measurements;
-impl Measurements {
-    pub fn compute(dpi: f32, value: f32, me: &Measurement) -> f32 {
-        match me {
-            Measurement::PX => value,
-            Measurement::MM => dpi / 25.4 * value,
-            Measurement::CM => dpi / 2.54 * value,
-            Measurement::DM => dpi / 0.254 * value,
-            Measurement::M => dpi / 0.0254 * value,
-            Measurement::IN => dpi * value,
-            Measurement::FT => dpi * value / 12.0
-        }
-    }
-}
-
+#[derive(Copy, Clone, Eq, PartialEq, Default)]
 pub enum Measurement {
+    #[default]
     PX,
     MM,
     CM,
@@ -21,4 +8,18 @@ pub enum Measurement {
     M,
     IN,
     FT
+}
+
+impl Measurement {
+    pub fn compute(&self, dpi: f32, value: f32) -> f32 {
+        match self {
+            Measurement::PX => value,
+            Measurement::MM => dpi / 25.4 * value,
+            Measurement::CM => dpi / 2.54 * value,
+            Measurement::DM => dpi / 0.254 * value,
+            Measurement::M => dpi / 0.0254 * value,
+            Measurement::IN => dpi * value,
+            Measurement::FT => dpi * 12.0 * value
+        }
+    }
 }
