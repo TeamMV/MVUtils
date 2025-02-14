1e8735d5a8c9f29e896a46da511872ec0f64b2b8
mqxf <mqxf@mvteam.dev>
2025-02-14 23:38:06 +0100

    integer decode

--- src/utils.rs
+++ src/utils.rs
@@ -611,6 +611,20 @@ pub fn format_escaped(input: &str) -> String {
     output
 }
 
+pub fn integer_decode(val: f64) -> (u64, i16, i8) {
+    let bits: u64 = unsafe { std::mem::transmute(val) };
+    let sign: i8 = if bits >> 63 == 0 { 1 } else { -1 };
+    let mut exponent: i16 = ((bits >> 52) & 0x7ff) as i16;
+    let mantissa = if exponent == 0 {
+        (bits & 0xfffffffffffff) << 1
+    } else {
+        (bits & 0xfffffffffffff) | 0x10000000000000
+    };
+
+    exponent -= 1023 + 52;
+    (mantissa, exponent, sign)
+}
+
 /// `fn_for` macro.
 ///
 /// This macro is used to generate functions that can be called directly from wrappers around
